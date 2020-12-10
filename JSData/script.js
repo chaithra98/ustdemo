f66506a7d0f2177adce2e2fcbd264b9ab13009f1
unknown <chaithrapgowda98@gmail.com>
2020-12-10 23:15:12 +0530

    CRUD_Chaithra.pptx

---
+++ JSData/script.js
@@ -0,0 +1,33 @@
+var tv = {
+	color: "grey",
+	
+	price:46999,
+	video_featues: {
+		led_display: "edge LED",
+		refresh rate: "60hz"
+	}
+	console.log(tv.color)
+}
+
+
+
+{
+	"departureAirport": "MUC",
+	"arrivalAirport": "SFO",
+	"aircraft": "Airbus A380",
+	"distance": 1200,
+	"intercontinental": true
+}
+{
+	"departureAirport": "LHR",
+	"arrivalAirport": "TXL",
+	"aircraft": "Airbus A360",
+	"distance": 6000,
+	"intercontinental": false
+}
+
+
+
+
+
+  
